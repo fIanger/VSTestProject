20cc7afd5c09553ffde04ffcbe56d79d7a887a9e
fIanger <cshilov@gmail.com>
2015-06-01 09:37:36 +0500

    sdf

--- TestProject/Form1.Designer.cs
+++ TestProject/Form1.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(920, 499);
+            this.button1.Location = new System.Drawing.Point(323, 130);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(672, 392);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
